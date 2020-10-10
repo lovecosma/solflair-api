3dd6a88d31dd200b8200cf71a6c73b532f835896
lovecosma <williamsmatthew333@gmail.com>
2020-10-09 23:48:27 -0600

    Completed user authentication and items components/ persisting login through refresh

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 post '/login',    to: 'sessions#create'
 post '/logout',   to: 'sessions#destroy'
-get '/logged_in', to: 'sessions#is_logged_in?'
+get '/logged_in', to: 'sessions#is_logged_in'
 resources :items, only: [:create, :show, :index, :destroy]
 resources :users, only: [:create, :show, :index] do 
  end
