77d3d8a4a0acf18743cf2531f4d711a398b174a4
lovecosma <williamsmatthew333@gmail.com>
2020-10-13 14:49:52 -0600

    Added functionality to account for duplicate items in carts and display quantities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3020_10_10_161356) do
+ActiveRecord::Schema.define(version: 3020_10_10_161357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 3020_10_10_161356) do
     t.integer "cart_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "quantity"
   end
 
   create_table "carts", force: :cascade do |t|
