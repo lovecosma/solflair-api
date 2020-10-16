8c06b9084019a23cd9296233a514e220dd69cab5
lovecosma <williamsmatthew333@gmail.com>
2020-10-16 14:50:46 -0600

    Debugged some nested resource things. Debugged access modification

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,6 +46,6 @@ class ItemsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def item_params
-      params.require(:item).permit(:name, :price)
+      params.require(:item).permit(:name, :price, :photo)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   post '/login',    to: 'sessions#create'
+  post '/photos',    to: 'photos#create'
   post '/logout',   to: 'sessions#destroy'
   get '/logged_in', to: 'sessions#is_logged_in'
   resources :items, only: [:create, :show, :index]
