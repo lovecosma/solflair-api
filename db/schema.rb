dbbb62370368814d689859e35b33ca0d88eb594a
lovecosma <williamsmatthew333@gmail.com>
2020-10-16 16:40:18 -0600

    added functionality for cartitem to display pictures

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_16_180810) do
+ActiveRecord::Schema.define(version: 2020_10_16_214607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_10_16_180810) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "photo"
   end
 
   create_table "items", force: :cascade do |t|
